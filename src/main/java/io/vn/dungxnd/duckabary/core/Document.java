282212627a84692875b92763b335b2817c82b58c
le hong duc <158602337+smeb2k5uetttt@users.noreply.github.com>
2024-10-07 23:05:33 +0700

    Upload Doc, DocMgr classes
    Add files via upload
    
    Co-Authored-By: le hong duc <158602337+smeb2k5uetttt@users.noreply.github.com>

--- src/main/java/io/vn/dungxnd/duckabary/core/Document.java
+++ src/main/java/io/vn/dungxnd/duckabary/core/Document.java
@@ -1,4 +1,41 @@
 package io.vn.dungxnd.duckabary.core;
 
 public class Document {
+    protected String title;
+    protected String author;
+    protected String id;
+    protected int quantity;
+
+    public Document(String title, String author, String id, int quantity)
+    {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
 }
+
