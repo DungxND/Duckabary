47f92e04f26d55e5bf7bd36c10c43d06f81389bc
le hong duc <158602337+smeb2k5uetttt@users.noreply.github.com>
2024-10-07 23:19:30 +0700

    Add files via upload

--- src/main/java/io/vn/dungxnd/duckabary/core/DocumentManagement.java
+++ src/main/java/io/vn/dungxnd/duckabary/core/DocumentManagement.java
@@ -1,5 +1,45 @@
 package io.vn.dungxnd.duckabary.core;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+// chuwa xong 
 public class DocumentManagement {
+    private ArrayList<Document> docm;
 
+    public DocumentManagement()
+    {
+        docm = new ArrayList<>();
+    }
+    public void addDocument(Document doc)
+    {
+        docm.add(doc);
+    }
+
+    public void removeDocument(String id)
+    {
+        docm.removeIf(doc -> doc.getId().equals(id));
+    }
+
+    public void borrowDOC(String docId)
+    {
+        for (Document doc : docm)
+        {
+            if (doc.getId().equals(docId))
+            {
+                if (doc.getQuantity() > 0)
+                {
+                    doc.setQuantity(doc.getQuantity() - 1);
+                    System.out.println("Document borrowed successfully");
+                }
+                else
+                {
+                    System.out.println("Document is not available");
+                }
+                return;
+            }
+        }
+        System.out.println("Document not found :))");
+    }
+
+    // chuaw xong
 }
