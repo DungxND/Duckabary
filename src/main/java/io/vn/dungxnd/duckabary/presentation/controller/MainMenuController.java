2f69eb2e744d2937e41ece1689540472a0f131da
duc <23021531@vnu.edu.vn>
2024-11-29 23:46:13 +0700

    chinh lai giao dien

--- src/main/java/io/vn/dungxnd/duckabary/presentation/controller/MainMenuController.java
+++ src/main/java/io/vn/dungxnd/duckabary/presentation/controller/MainMenuController.java
@@ -9,7 +9,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.FileChooser;
 
 
@@ -70,13 +72,43 @@ public class MainMenuController implements Initializable {
     @FXML
     private Button allButton;
 
+    @FXML
+    private SVGPath docSvg;
+
+    @FXML
+    private SVGPath authorSvg;
+
+    @FXML
+    private SVGPath publishSvg;
+
+    @FXML
+    private SVGPath leftUserSvg;
+
+    @FXML
+    private SVGPath leftLibSvg;
+
+    @FXML
+    private SVGPath leftBorSvg;
+
+    @FXML
+    private AnchorPane leftBorIconPane;
+
+    @FXML
+    private AnchorPane leftLibIconPane;
+
+    @FXML
+    private AnchorPane leftUserIconPane;
+
     public void enterDocManage() {
         docList.setVisible(true);
         authorList.setVisible(false);
         publisherList.setVisible(false);
-        docIcon.setStyle("-fx-background-color: grey");
+        docIcon.setStyle("-fx-background-color: #3A3026");
         authorIcon.setStyle("-fx-background-color: #FFFFFF");
         publishIcon.setStyle("-fx-background-color: #FFFFFF");
+        docSvg.setFill(Color.WHITE);
+        authorSvg.setFill(Color.BLACK);
+        publishSvg.setFill(Color.BLACK);
         libTitle.setText("Document List");
 
     }
@@ -85,8 +117,11 @@ public class MainMenuController implements Initializable {
         publisherList.setVisible(false);
         docList.setVisible(false);
         docIcon.setStyle("-fx-background-color: #FFFFFF");
-        authorIcon.setStyle("-fx-background-color: grey");
+        authorIcon.setStyle("-fx-background-color: #3A3026");
         publishIcon.setStyle("-fx-background-color: #FFFFFF");
+        docSvg.setFill(Color.BLACK);
+        authorSvg.setFill(Color.WHITE);
+        publishSvg.setFill(Color.BLACK);
         libTitle.setText("Author List");
     }
 
@@ -96,7 +131,10 @@ public class MainMenuController implements Initializable {
         docList.setVisible(false);
         docIcon.setStyle("-fx-background-color: #FFFFFF");
         authorIcon.setStyle("-fx-background-color: #FFFFFF");
-        publishIcon.setStyle("-fx-background-color: grey");
+        publishIcon.setStyle("-fx-background-color: #3A3026");
+        docSvg.setFill(Color.BLACK);
+        authorSvg.setFill(Color.BLACK);
+        publishSvg.setFill(Color.WHITE);
         libTitle.setText("Publisher List");
     }
 
@@ -105,6 +143,12 @@ public class MainMenuController implements Initializable {
         brBg.setVisible(false);
         libBg.setVisible(true);
         settingBg.setVisible(false);
+        leftLibIconPane.setStyle("-fx-background-color: #9D9D95");
+        leftBorIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftUserIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftLibSvg.setFill(Color.WHITE);
+        leftBorSvg.setFill(Color.BLACK);
+        leftUserSvg.setFill(Color.BLACK);
     }
 
     public void enterUserManage() {
@@ -112,6 +156,12 @@ public class MainMenuController implements Initializable {
         brBg.setVisible(false);
         userBg.setVisible(true);
         settingBg.setVisible(false);
+        leftLibIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftBorIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftUserIconPane.setStyle("-fx-background-color: #9D9D95");
+        leftLibSvg.setFill(Color.BLACK);
+        leftBorSvg.setFill(Color.BLACK);
+        leftUserSvg.setFill(Color.WHITE);
     }
 
     public void enterBorrowManage() {
@@ -119,6 +169,12 @@ public class MainMenuController implements Initializable {
         userBg.setVisible(false);
         brBg.setVisible(true);
         settingBg.setVisible(false);
+        leftLibIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftBorIconPane.setStyle("-fx-background-color: #9D9D95");
+        leftUserIconPane.setStyle("-fx-background-color: #E0DFDC");
+        leftLibSvg.setFill(Color.BLACK);
+        leftBorSvg.setFill(Color.WHITE);
+        leftUserSvg.setFill(Color.BLACK);
     }
 
     public void enterSetting() {
@@ -143,17 +199,17 @@ public class MainMenuController implements Initializable {
         if (selectedFile != null) {
             try {
                 Circle clip = new Circle();
-                clip.setCenterX(avaButton.getFitWidth() / 2);
+                clip.setCenterX(avaButton.getFitWidth() / 2 );
                 clip.setCenterY(avaButton.getFitHeight() / 2);
-                clip.setRadius(Math.min(avaButton.getFitWidth(), avaButton.getFitHeight()) / 2);
-                avaButton.setClip(clip);
+                clip.setRadius(Math.min(avaButton.getFitWidth(), avaButton.getFitHeight()) *17/40);
                 Image avatarImage = new Image(selectedFile.toURI().toString());
                 avaButton.setImage(avatarImage);
+                avaButton.setClip(clip);
 
                 Circle clip1 = new Circle();
                 clip1.setCenterX(setAva.getFitWidth() / 2);
                 clip1.setCenterY(setAva.getFitHeight() / 2);
-                clip1.setRadius(Math.min(setAva.getFitWidth(), setAva.getFitHeight()) / 2);
+                clip1.setRadius(Math.min(setAva.getFitWidth(), setAva.getFitHeight()) *17 /40);
                 setAva.setClip(clip1);
                 setAva.setImage(avatarImage);
 
@@ -172,18 +228,21 @@ public class MainMenuController implements Initializable {
         Circle clip = new Circle();
         clip.setCenterX(avaButton.getFitWidth() / 2);
         clip.setCenterY(avaButton.getFitHeight() / 2);
-        clip.setRadius(Math.min(avaButton.getFitWidth(), avaButton.getFitHeight()) / 2);
+        clip.setRadius(Math.min(avaButton.getFitWidth(), avaButton.getFitHeight()) *17/ 40);
         avaButton.setClip(clip);
 
         Circle clip1 = new Circle();
         clip1.setCenterX(setAva.getFitWidth() / 2);
         clip1.setCenterY(setAva.getFitHeight() / 2);
-        clip1.setRadius(Math.min(setAva.getFitWidth(), setAva.getFitHeight()) / 2);
+        clip1.setRadius(Math.min(setAva.getFitWidth(), setAva.getFitHeight()) *17/40);
         setAva.setClip(clip1);
 
-        docIcon.setStyle("-fx-background-color: grey");
+        docIcon.setStyle("-fx-background-color: #3A3026");
         authorIcon.setStyle("-fx-background-color: #FFFFFF");
         publishIcon.setStyle("-fx-background-color: #FFFFFF");
+        docSvg.setFill(Color.WHITE);
 
+        leftLibIconPane.setStyle("-fx-background-color: #9D9D95");
+        leftLibSvg.setFill(Color.WHITE);
     }
 }
